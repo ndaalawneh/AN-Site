9976e8da32759dba38cb3a8af07eb06906b4a82c
ndaalawneh <ndaalawneh@gmail.com>
2022-04-09 16:45:43 +0300

    edit what i done

--- js/signup.js
+++ js/signup.js
@@ -1,14 +1,13 @@
-
 var letters = /^[a-zA-Z]/g;
-var m= /^\w+([\.-]\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/ ;
+var m = /^\w+([\.-]\w+)@\w+([\.-]?\w+)(\.\w{2,3})+$/;
 
 var p1 = /\W/;
-var p2 = /\w/ ;
-var p3 = /\d/ ;
+var p2 = /\w/;
+var p3 = /\d/;
 
 
 
-function Regestration(){
+function Regestration() {
 
     var fname = document.getElementById('fname').value;
     var lname = document.getElementById('lname').value;
@@ -17,55 +16,106 @@ function Regestration(){
     var email = document.getElementById('email').value;
     var cemail = document.getElementById('cemail').value;
 
-  
-    
 
 
-
-    if (fname =='' || lname== '') {
+    var op = true;
+if (! m.test(email))
+    if (fname == '' || lname == '') {
         alert('Please Enter Your Name ');
-       
-    }
-    
-    if (fname.match(letters) && lname.match(letters)) {
-        alert('The Name Has Been Accepted')
+        op = false
     }
 
-    if(password ==''){
+
+    if (password == '') {
         alert('Please Enter Your Password')
+        op = false
     }
-    
-    if(!password.match(p1)){
+
+    if (!password.match(p1)) {
         alert('please enter at least one special character')
+        op = false
     }
 
-    if(!password.match(p2)){
+    if (!password.match(p2)) {
         alert('you should enter at least one letter')
+        op = false
     }
 
-    if(!password.match(p3)){
+    if (!password.match(p3)) {
         alert('you should enter at least one number ')
+        op = false
     }
-   
-    if(password.length<8){
-        alert ('Password minimum length is 8');
-    }
-    
 
-    if(!password.match(cpassword)){
+    if (password.length < 8) {
+        alert('Password minimum length is 8');
+        op = false
+    }
+
+
+    if (password != cpassword) {
         alert('Check Your Password Please')
+        op = false
     }
 
-    if(email==''){
+    if (email == '') {
         alert('Please Enter Your Email')
+        op = false
     }
 
-   
-     if(!email.match(m)){
-         alert('Invailed Email')
-     }
 
-     if(!email.match(cemail)){
-         alert('Check Youe Email Please')
-     }
+  
+
+    if (email != cemail) {
+        alert('Check Youe Email Please')
+        op = false
+    }
+    if (localStorage.getItem("users") == null) {
+        savedata()
+    }
+    else {
+        if (Checkiemail(email)) {
+            alert("user existe ")
+        }
+        else {
+            if (op) {
+
+
+                savedata();
+            }
+        }
+    }
+}
+function User(name, email, password) {
+    this.name = name
+    this.email = email
+    this.password = password
+}
+
+function Checkiemail(e) {
+    var users = JSON.parse(localStorage.getItem("users"))
+    for (i = 0; i < users.length; i++) {
+        var user = users[i]
+        if (user.email == e) {
+            return true
+        }
+    }
+}
+function savedata() {
+    var fname = document.getElementById('fname').value;
+    var lname = document.getElementById('lname').value;
+    var password = document.getElementById('pass').value;
+    var cpassword = document.getElementById('cpass').value;
+    var email = document.getElementById('email').value;
+    var cemail = document.getElementById('cemail').value;
+
+
+    var a = []
+    if (localStorage.getItem("users") === null) {
+        localStorage.setItem("users", JSON.stringify(a))
+    }
+    var user = new User(fname + " " + lname, email, password)
+    var users = JSON.parse(localStorage.getItem("users"))
+    users.push(user)
+    localStorage.setItem("users", JSON.stringify(users))
+
 }
